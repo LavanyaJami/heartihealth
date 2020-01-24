6b6b086e7b4ab1fc3e05efbdd4bf490ffb02c6da
Shobana.A <Shobana.A@PT-MIL-005.Pratian.com>
2020-01-24 09:56:36 +0530

    Solved the errors & added dependency

--- src/test/java/com/pratian/heartihealth/BaseClass.java
+++ src/test/java/com/pratian/heartihealth/BaseClass.java
@@ -6,22 +6,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
-
-	public WebDriver driver;
-	private final String chromeDriverPath = ".\\src\\main\\resources\\drivers\\chromedriver.exe";
-	// private final String
-	// firefoxDriverPath=".\\src\\main\\resources\\drivers\\geckodriver-v0.26.0-win32\\geckodriver.exe";
-
-	@BeforeMethod
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-	}
 	
-	@AfterMethod
-	public void teardown()
-	{
-		driver.close();
+		public ChromeDriver driver;
+		private final String chromeDriverPath = "/heartihealth/src/main/resources/drivers/chromedriver.exe";
+		
+		
+		@BeforeMethod
+		public void setUp() {
+			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		
+		@AfterMethod
+		public void teardown()
+		{
+			driver.close();
+			
+		}
 	}
-}
+
+
+
